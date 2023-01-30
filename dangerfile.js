7e27da9f178cb9163173e78166266b5860b355e9
medev21 <benmarteen07@gmail.com>
2023-01-29 23:21:59 -0500

    refactor: update dangerjs pr validation

--- dangerfile.js
+++ dangerfile.js
@@ -1,14 +1,38 @@
-// Add a CHANGELOG entry for app changes
-const hasChangelog = danger.git.modified_files.includes('changelog.md');
-const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes(
-    '#trivial'
-);
-if (!hasChangelog && !isTrivial) {
-    warn('Please add a changelog entry for your changes here.');
+import {danger, warn, fail} from 'danger'
+
+/**
+ * Check if it's a large pull request
+ */
+const bigPRThreshold = 600; //lines
+
+if(danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+    warn("Big pull request, please try to keep small PRs so it's easier to review :)")
 }
 
 /**
- * TODO: ADD PR TITLE CHECK
- * CHECK FOR NUMBER OF FILES TO REVIEW - WARN
- * CHECK PR TITLE FORMAT - DONT PASS ON FAILURE
+ * Check if PR title follows expected format
+ * with the following change types
+ * 
+ * example format:
+ * "feat: <PR title>"
  */
+const changeType = [
+    'build',
+    'chore',
+    'ci',
+    'docs',
+    'feat',
+    'fix',
+    'perf',
+    'refactor',
+    'revert',
+    'style',
+    'test',
+].join("|");
+
+const prTitlePrefix = `^(${changeType}): .+`;
+const regexp = new RegExp(prTitlePrefix, "g");
+
+if(!regexp.test(danger.github.pr.title)) {
+    fail("Please change the PR title format")
+}
